9be223262f0dd4fe87193e46de53b6cf975922c1
German <g.gabrielpuleo@gmail.com>
2025-01-16 12:54:32 +0100

    component creation and added tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+enum Pages {
+    Home = 'home',
+    Detail = 'detail'
+}
+
+export const routes: Routes = [
+    {
+        path: Pages.Home,
+        loadComponent: () => import('./pages/home/home.component').then((page) => page.HomeComponent)
+    },
+    {
+        path: Pages.Detail,
+        loadComponent: () => import('./pages/detail/detail.component').then((page) => page.DetailComponent)
+    },
+    {
+        path: '**',
+        redirectTo: Pages.Home,
+        pathMatch: 'full'
+    }
+];
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Scan all HTML and TypeScript files for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
